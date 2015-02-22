79a963aa675e2218520fd2cf3b4e4eadeac18a19
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2015-02-22 15:49:00 +0100

    Removed Wrapper as it did the same as Reference. Reference: toString and clone were removed as they doesn't make sense for the intended usage. Changed the name of the primitive equivalents so they don't conflict with the boxed types.

--- Utils/src/tbm/util/Reference.java
+++ Utils/src/tbm/util/Reference.java
@@ -1,87 +1,100 @@
 package tbm.util;
 import java.io.Serializable;
 
-//A simple wrapper for passing an immutable object by reference.
-public class Reference<T> implements Serializable{
+/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+public class Reference<T> implements Serializable {
+	/**The referenced value.*/          	public T value;
+	/**@param value the initial value.*/	public Reference(T value)	{set(value);}
+	/**@return the referenced value.*/  	public T get()           	{return this.value;}
+	/**@return the new value.*/         	public T set(T value)    	{this.value = value; return get();}
 	private static final long serialVersionUID = 1L;
-	public T value;
-	public Reference(T value)	{this.value = value;}
-	public T get()				{return this.value;}
-	public void set(T value)	{this.value = value;}
-	public String toString()	{return String.valueOf(value);}
 
-	/*
-	public static class T implements Serializable{
-		public T(T value)	{this.value = value;}
+	/**@return a new reference to the value.*/public static <T> Reference<T> to(T value)	{return new Reference<T>(value);}
+	/**@return a new reference to the value.*/public static Reference_boolean to(boolean value)	{return new Reference_boolean(value);}
+	/**@return a new reference to the value.*/public static Reference_char to(char value)	{return new Reference_char(value);}
+	/**@return a new reference to the value.*/public static Reference_byte to(byte value)	{return new Reference_byte(value);}
+	/**@return a new reference to the value.*/public static Reference_short to(short value)	{return new Reference_short(value);}
+	/**@return a new reference to the value.*/public static Reference_int to(int value)	{return new Reference_int(value);}
+	/**@return a new reference to the value.*/public static Reference_long to(long value)	{return new Reference_long(value);}
+	/**@return a new reference to the value.*/public static Reference_float to(float value)	{return new Reference_float(value);}
+	/**@return a new reference to the value.*/public static Reference_double to(double value)	{return new Reference_double(value);}
+	///**@return a new reference to the value.*/public static Reference_T to(T value)	{return new Reference_T(value);}
+
+
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_boolean implements Serializable {
+		/**The referenced value.*/          	public boolean value;
+		/**@param value the initial value.*/	public Reference_boolean(boolean value)	{set(value);}
+		/**@return the referenced value.*/  	public boolean get()             	{return this.value;}
+		/**@return the new value.*/         	public boolean set(boolean value)  	{this.value = value; return get();}
 		private static final long serialVersionUID = 1L;
-		public T value;
-		public T get()				{return this.value;}
-		public void set(T value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
+	}
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_char implements Serializable {
+		/**The referenced value.*/          	public char value;
+		/**@param value the initial value.*/	public Reference_char(char value)	{set(value);}
+		/**@return the referenced value.*/  	public char get()          	{return this.value;}
+		/**@return the new value.*/         	public char set(char value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_byte implements Serializable {
+		/**The referenced value.*/          	public byte value;
+		/**@param value the initial value.*/	public Reference_byte(byte value)	{set(value);}
+		/**@return the referenced value.*/  	public byte get()          	{return this.value;}
+		/**@return the new value.*/         	public byte set(byte value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_short implements Serializable {
+		/**The referenced value.*/          	public short value;
+		/**@param value the initial value.*/	public Reference_short(short value)	{set(value);}
+		/**@return the referenced value.*/  	public short get()           	{return this.value;}
+		/**@return the new value.*/         	public short set(short value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_int implements Serializable {
+		/**The referenced value.*/          	public int value;
+		/**@param value the initial value.*/	public Reference_int(int value)	{set(value);}
+		/**@return the referenced value.*/  	public int get()         	{return this.value;}
+		/**@return the new value.*/         	public int set(int value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_long implements Serializable {
+		/**The referenced value.*/          	public long value;
+		/**@param value the initial value.*/	public Reference_long(long value)	{set(value);}
+		/**@return the referenced value.*/  	public long get()          	{return this.value;}
+		/**@return the new value.*/         	public long set(long value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_float implements Serializable {
+		/**The referenced value.*/          	public float value;
+		/**@param value the initial value.*/	public Reference_float(float value)	{set(value);}
+		/**@return the referenced value.*/  	public float get()           	{return this.value;}
+		/**@return the new value.*/         	public float set(float value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*/
+	public static class Reference_double implements Serializable {
+		/**The referenced value.*/          	public double value;
+		/**@param value the initial value.*/	public Reference_double(double value)	{set(value);}
+		/**@return the referenced value.*/  	public double get()            	{return this.value;}
+		/**@return the new value.*/         	public double set(double value)	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
+	}
+
+	/*Quicker to copy/paste and search/replace than doing every change in eight places
+	/**A simple wrapper for passing a value from a lambda to an outer scope or updating a returned immutable value.*
+	public static class Reference_T implements Serializable {
+		/**The referenced value.*          	public T value;
+		/**@param value the initial value.*	public Reference_T(T value)	{set(value);}
+		/**@return the referenced value.*  	public T get()           	{return this.value;}
+		/**@return the new value.*         	public T set(T value)    	{this.value = value; return get();}
+		private static final long serialVersionUID = 1L;
 	}*/
-
-	public static class Boolean implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public boolean value;
-		public Boolean(boolean value)	{this.value = value;}
-		public boolean get()				{return this.value;}
-		public void set(boolean value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Char implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public char value;
-		public Char(char value)	{this.value = value;}
-		public char get()				{return this.value;}
-		public void set(char value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Byte implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public byte value;
-		public Byte(byte value)	{this.value = value;}
-		public byte get()				{return this.value;}
-		public void set(byte value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Short implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public short value;
-		public Short(short value)	{this.value = value;}
-		public short get()				{return this.value;}
-		public void set(short value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Int implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public int value;
-		public Int(int value)	{this.value = value;}
-		public int get()				{return this.value;}
-		public void set(int value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Long implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public long value;
-		public Long(long value)	{this.value = value;}
-		public long get()				{return this.value;}
-		public void set(long value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Float implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public float value;
-		public Float(float value)	{this.value = value;}
-		public float get()				{return this.value;}
-		public void set(float value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
-	public static class Double implements Serializable{
-		private static final long serialVersionUID = 1L;
-		public double value;
-		public Double(double value)	{this.value = value;}
-		public double get()				{return this.value;}
-		public void set(double value)	{this.value = value;}
-		public String toString()	{return String.valueOf(value);}
-	}
 }
