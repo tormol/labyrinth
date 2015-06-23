640834138afb25beef2f9e051833964e13545f99
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2015-06-24 01:10:16 +0200

    parseNum: before flags. (errors)

--- Utils/src/tbm/util/parseNum.java
+++ Utils/src/tbm/util/parseNum.java
@@ -1,6 +1,6 @@
 package tbm.util;
 
-/**Parsing numbers from a stream*/
+/**Parsing numbers from a stream, with the option of accepting other number systems, or ignoring spaces, tabs or any characters you want.*/
 public class parseNum {
 	private String spaces = null;
 	private boolean negative = false;
@@ -55,11 +55,29 @@ public class parseNum {
 	}
 
 
-	public static <EX extends Throwable> long signed_long(CharSupplier<EX> ch, boolean other_systems, String spaces) throws EX, NumberFormatException {
-		return parse_signed(ch, Long.SIZE, other_systems, spaces);
+	  //****************//
+	 // static methods //
+	//****************//
+
+
+
+
+	public static <EX extends Throwable> long any_long(boolean other_systems, String spaces, CharSupplier<EX> ch) throws EX, NumberFormatException {
+		return bits(Long.SIZE, ch, other_systems, spaces);
 	}
-	public static <EX extends Throwable> long unsigned_long(CharSupplier<EX> ch, boolean negative, boolean other_systems, String spaces) throws EX, NumberFormatException {
-		return parse(ch, ch.get(), Long.SIZE, false, negative, other_systems, spaces);
+	public static <EX extends Throwable> long bits(int bits, boolean negative, boolean can_overflow, String spaces, CharSupplier<EX> ch) throws EX, NumberFormatException {
+		return parse(ch, ch.fetch(), Long.SIZE, false, negative, other_systems, spaces);
+	}
+	private static <EX extends Throwable> long bits(int bits, boolean negative, boolean can_overflow, boolean other_systems, String spaces, CharSupplier<EX> ch) throws EX, NumberFormatException {
+		return parse(ch, ch.fetch(), Long.SIZE, false, negative, other_systems, spaces);
+	}
+	public static <EX extends Throwable> long range_long(CharSupplier<EX> ch, long min, long max, boolean other_systems, String spaces) throws EX, NumberFormatException, IllegalArgumentException {
+		long n = min < 0
+			? signed_long(ch, other_systems, spaces)
+			: unsigned_long(ch, false, other_systems, spaces);
+		if (n < min  ||  n > max)
+			throw new NumberFormatException(n +" is outside the accepted range of ["+min+", "+max);
+		return n;
 	}
 
 	public static <EX extends Throwable> int signed_int(CharSupplier<EX> ch, boolean other_systems, String spaces) throws EX, NumberFormatException {
@@ -87,7 +105,7 @@ public class parseNum {
 
 	private static <EX extends Throwable> int next(CharSupplier<EX> ch, String skip) throws EX {
 		int c;
-		do c = ch.get();
+		do c = ch.fetch();
 			while (skip.indexOf(c) != -1);
 		return c;
 	}
@@ -95,27 +113,33 @@ public class parseNum {
 	private static <EX extends Throwable> long parse_signed(CharSupplier<EX> ch, int bits, boolean other_systems, String spaces) throws EX, NumberFormatException {
 		int first = next(ch, spaces);
 		if (first == '-')
-			return parse(ch, ch.get(), bits, true, true, other_systems, spaces);
+			return parse(ch, ch.fetch(), bits, true, true, other_systems, spaces);
 		return parse(ch, first, bits, true, false, other_systems, spaces);
 	}
-	private static <EX extends Throwable> long parse_unsigned(CharSupplier<EX> ch, int bits, boolean negative, boolean other_systems, String spaces) throws EX, NumberFormatException {
-		return parse(ch, ch.get(), bits, false, negative, other_systems, spaces);
+	private static <EX extends Throwable>
+			long parse_unsigned(
+					CharSupplier<EX> ch, int bits, boolean negative,
+					boolean other_systems, String spaces) throws EX, NumberFormatException {
+		return parse(ch, ch.fetch(), bits, false, negative, other_systems, spaces);
 	}
 	
 
-	/**@param ch input stream
-	 * @param c holds the first digit
-	 * @param bits number of bits in the number, int because <Boxed>.SIZE is int
-	 * @param signed should the first bit in bits be used for sign
-	 * @param negative is this number negative
-	 * @param other_systems are binary(0b), hexadecimal(0x) or octal(0o) numbers allowed?
-	 * @param spaces a list of characters that wont stop parsing, but will be ignored. Cannot be null.
-	 * detects base, compute max value and max digits
+	/**detects base, compute max value and max digits
+	 *@param ch input stream
+	 *@param c holds the first digit
+	 *@param bits number of bits in the number, int because <Boxed>.SIZE is int
+	 *@param can_overflow can negative numbers be entered as positive
+	 *@param negative is this number negative
+	 *@param other_systems are binary(0b), hexadecimal(0x) or octal(0o) numbers allowed?
+	 *@param spaces a list of characters that won't stop parsing, but will be ignored. Cannot be null.
+	 *@exception EX thrown by CharSupplier
+	 *@throws NumberFormatException NaN/no digits, too big/small/long 
+	 *@throws IllegalArgumentException if bits is not between 1 and Long.SIZE
 	 */
 	private static <EX extends Throwable> long parse (
-				CharSupplier<EX> ch, int c, int bits, boolean signed,
+				CharSupplier<EX> ch, int c, int bits, boolean can_overflow,
 				boolean negative, boolean other_systems, String spaces
-			) throws EX {
+			) throws EX, IllegalArgumentException, NumberFormatException {
 		byte base=10;
 		byte digits=0;
 		byte max_digits = -1;//not used with base 10;
@@ -140,11 +164,11 @@ public class parseNum {
 			throw new IllegalArgumentException("Cannot store more than "+Long.SIZE+" bits.");
 
 		if (base != 10) {
-			c = ch.get();
+			c = ch.fetch();
 			int base_bits = Integer.highestOneBit(base)-1;
 			//round up http://stackoverflow.com/questions/7446710/how-to-round-up-integer-division-and-have-int-result-in-java
 			max_digits = (byte) ((bits-1+base_bits)/base_bits);
-		} else if (signed) {
+		} else if ( !can_overflow) {
 			max_value >>= 1;
 			if (negative)
 				max_value++;
@@ -153,10 +177,22 @@ public class parseNum {
 	}
 
 
+	/**Do the actual parsing.
+	 *@param ch character source
+	 *@param c the first char, can't rewind CharSupplier
+	 *@param base which characters are digits?
+	 *@param negative is the returned number negative, doesn't affect parsing as parse() checked for minus sign.
+	 *@param max_digits maximum number of digits, including leading zeroes, multiplying a number by more than two and make it hard to detect.
+	 *@param digits number of digits already read.
+	 *@param max_value max allowed value, should be negative.
+	 *@param spaces a list of characters that won't stop parsing, but will be ignored. Cannot be null.
+	 *@exception EX thrown by CharSupplier
+	 *@throws NumberFormatException NaN/no digits, too big/small/long 
+	 *///Uses negative numbers to Allow Long.MIN_VALUE
 	private static <EX extends Throwable> long do_parse (
 				CharSupplier<EX> ch, int c, byte base, boolean negative,
 				byte max_digits, int digits, long max_value, String spaces
-			) throws EX {
+			) throws EX, NumberFormatException {
 		long num=0;
 		//when reading a unsigned long a too big value would be less than ~0, so comparing doesn't work
 		//my workaround is to set this to true when num overflows and become negative, and check if num becomes positive again.
@@ -165,20 +201,21 @@ public class parseNum {
 			int n = digit(base, c);
 			if (n == -1)
 				if (spaces.indexOf(c) != -1) {
-					c = ch.get();
+					c = ch.fetch();
 					continue;
 				} else
 					break;
 			digits++;
-			num = num*base + n;
-			c = ch.get();
+			num = num*base - n;
+			c = ch.fetch();
 			if (base != 10  &&  digits > max_digits)
-				throw new NumberFormatException("Too many digits, "+max_digits+" is max.");
+				throw new NumberFormatException(numToString(num, base, negative)+" has more than "+max_digits+" digits.");
 			if (num < 0)
 				overflowed = true;
 			//wrapped around from negative to positive, aka 64bit unsigned overflow
 			else if (overflowed  ||  Long.compareUnsigned(num, max_value) > 0) {
 				int bits = (int)Math.ceil( Math.log(max_value) / Math.log(2) );//ln n/ln 2=log2 n
+				Long.n
 				if (negative)
 					throw new NumberFormatException(-num+" is too small for "+bits+" bits.");
 				else
@@ -186,11 +223,8 @@ public class parseNum {
 			}
 		}
 		if (digits == 0)
-			if (base == 10)//no digits
-				throw new NumberFormatException("Not a number");
-			else//0xbo and then nothing
-				throw new NumberFormatException("No digits");
-		if (negative)
+			throw new NumberFormatException("No digits");
+		if (!negative)
 			num = -num;
 		return num;
 	}
@@ -205,5 +239,22 @@ public class parseNum {
 		if (base == 16  &&  c >= 'a'  &&  c <= 'f')
 			return c+0xa-'a';
 		return -1;
+	}
+
+	/**so exception messages use the correct base and prepend 0[xob], and does unsigned correctly.*/
+	private static String numToString(long num, byte base, boolean negative) {
+		StringBuilder out = new StringBuilder(18);
+		if (negative)
+			out.append('-');
+		if (base != 10) {
+			out.append('0');
+			switch (base) {//gave up finding a formula, the switch is probably faster anyway
+				case  2: out.append('b'); break;//02=0b00010->b=098=0b01100010	+00=0b0000->+00=0b00000
+				case  8: out.append('o'); break;//16=0b10000->x=120=0b01111000	+14=0b1110->+22=0b10110
+				case 16: out.append('x'); break;//08=0b01000->o=111=0b01101111	+06=0b0110->+13=0b01101
+			}
+		}
+		out.append(Long.toUnsignedString(num, base));
+		return out.toString();
 	}
 }
--- Utils/tests/tbm/util/parseNumTest.java
+++ Utils/tests/tbm/util/parseNumTest.java
@@ -1,43 +1,57 @@
 package tbm.util;
-
 import static org.junit.Assert.*;
-
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class parseNumTest {
+	@Rule
+    public ExpectedException thrown= ExpectedException.none();
 
 	@Test
-	public void testParseNum() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAllow_negative_numbers() {
-		fail("Not yet implemented");
+	public void testLong_MIN_VALUE() {
+		String minLong = "-9223372036854775808";
+		assertEquals("wrong minLong", String.valueOf(Long.MIN_VALUE), minLong);
+		assertEquals(Long.MIN_VALUE, parseNum.signed_long(CharSupplier.fromString(minLong), false, ""));
+		assertEquals(Long.MIN_VALUE, parseNum.unsigned_long(CharSupplier.fromString(minLong.substring(1)), true, false, ""));
+		thrown.expect(NumberFormatException.class);
+		parseNum.signed_long(CharSupplier.fromString("-9223372036854775809"), false, "");
 	}
 
 	@Test
 	public void testAllow_other_number_systems() {
-		fail("Not yet implemented");
+		assertEquals("allowing other number systems doesn't break anything",
+				parseNum.signed_long(CharSupplier.fromString("010"), false, ""),
+				parseNum.signed_long(CharSupplier.fromString("010"), true, ""));
+		assertEquals("hexadecimal",
+				parseNum.signed_long(CharSupplier.fromString("10"), true, ""),
+				parseNum.signed_long(CharSupplier.fromString("0xa"), true, ""));
+		assertEquals("octal",
+				parseNum.signed_long(CharSupplier.fromString("10"), true, ""),
+				parseNum.signed_long(CharSupplier.fromString("0o12"), true, ""));
+		assertEquals("binary",
+				parseNum.signed_long(CharSupplier.fromString("10"), true, ""),
+				parseNum.signed_long(CharSupplier.fromString("0b1010"), true, ""));
+		assertEquals("other bases can overflow to negative without minus sign",
+				parseNum.signed_byte(CharSupplier.fromString("-1"), true, ""),
+				parseNum.signed_byte(CharSupplier.fromString("0xff"), true, ""));
+		thrown.expect(NumberFormatException.class);//no digits
+		parseNum.signed_long(CharSupplier.fromString("0x"), true, "");
 	}
 
 	@Test
-	public void testAllow_spaces() {
-		fail("Not yet implemented");
+	public void testWithSpaces() {
+		assertEquals("accepting unused characthers is ok",
+				parseNum.signed_long(CharSupplier.fromString("10"), false, ""),
+				parseNum.signed_long(CharSupplier.fromString("10"), false, " _"));
+		assertEquals("space characters are ignored",
+				parseNum.signed_long(CharSupplier.fromString("10"), false, " _"),
+				parseNum.signed_long(CharSupplier.fromString("1_ 0"), false, " _"));
 	}
 
-	@Test
-	public void testNo_spaces() {
-		fail("Not yet implemented");
-	}
 
-	@Test
+/*	@Test
 	public void testParse_long() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMain() {
 		fail("Not yet implemented");
 	}
 
@@ -85,5 +99,5 @@ public class parseNumTest {
 	public void testUnsigned_byte() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
