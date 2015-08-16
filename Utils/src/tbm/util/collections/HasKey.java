e50f561e9786136d64e086b4eddca4fbd9dd3472
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2015-08-16 16:30:51 +0200

    move AbstractHasKey into HasKey and add HasKey.CompleteHashKey

--- Utils/src/tbm/util/collections/HasKey.java
+++ Utils/src/tbm/util/collections/HasKey.java
@@ -1,5 +1,7 @@
 package tbm.util.collections;
+import java.util.Objects;
 
+//TODO javadoc
 /**When you want to use SetWithGet
  * This interface adds requirements to some Object methods:
  * 	hashCode() to be the same as getKey().hashCode()
@@ -8,4 +10,44 @@ package tbm.util.collections;
 public interface HasKey<K> {
 	/**Should never change nor return {@code null}*/
 	K getKey();
+
+
+
+	/**implements hashCode() and equals() correctly*/
+	public abstract class AbstractHasKey<K> implements HasKey<K> {
+		/***/@Override//Object
+		public final boolean equals(Object o) {
+			if (o == this)
+				return true;
+			if (o == null)
+				return false;
+			if (o.getClass() == this.getClass())
+				return getKey().equals(((AbstractHasKey<?>)o).getKey());
+			if (getKey().getClass().isAssignableFrom(o.getClass()))
+				return getKey().equals(o);
+			return false;
+		}
+	
+		/**@return {@code getKey().hashCode()}*/@Override//Object
+		public final int hashCode() {
+			return getKey().hashCode();
+		}
+	
+		/**@return {@code getKey().toString()}*/@Override//Object
+		public String toString() {
+			return getKey().toString();
+		}
+	}
+
+
+
+	public static class CompleteHasKey<K> extends AbstractHasKey<K> {
+		protected final K key;
+		protected CompleteHasKey(K key) {
+			this.key = Objects.requireNonNull(key);
+		}
+		@Override public final K getKey() {
+			return key;
+		}
+	}
 }
