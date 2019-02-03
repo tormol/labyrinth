04f30791a11d552ebb4e40b99d9df1e8f5ee44ae
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2019-02-03 14:54:41 +0100

    Add module-info files

---
+++ Utils/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module no.torbmol.util {
+    exports no.torbmol.util;
+    exports no.torbmol.util.collections;
+    exports no.torbmol.util.geom;
+
+    requires java.base;
+    requires java.desktop; // awtKeyListen, Point, statics
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module no.torbmol.labyrinth {
+    requires no.torbmol.util;
+    requires java.base;
+    requires java.desktop; // swing
+}
