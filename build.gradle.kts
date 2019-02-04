5147d28aaf7c8ea976b32aff11f481fc7f4afb5c
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2019-02-04 09:57:11 +0100

    Build labyrinth.jar with utils included
    ... with task shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     `java`
     application
+    // creates jar with dependencies included
+    id("com.github.johnrengelman.shadow") version "4.0.2"
     // generates .classpath and .project files. Run `gw eclipse`
     `eclipse`
 }
@@ -11,4 +13,17 @@ dependencies {
 
 application {
     mainClassName = "no.torbmol.labyrinth.Labyrinth"
+}
+
+// Don't invoke this task directly; use shadowjar to get a standalone jar
+val jar by tasks.existing(Jar::class) {
+    manifest {
+        attributes(mapOf(
+            "Main-Class" to application.mainClassName
+        ))
+    }
+}
+
+val shadowJar by tasks.existing(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
+    archiveName = "labyrinth.jar"
 }
