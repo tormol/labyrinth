3c8d7a3e6a674f24358325bff7eea1a27fc6e1b9
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2019-02-04 11:15:59 +0100

    Add license and copyright info

--- Utils/src/main/java/no/torbmol/util/collections/LeanHash.java
+++ Utils/src/main/java/no/torbmol/util/collections/LeanHash.java
@@ -1,3 +1,11 @@
+/* Copyright 2016 Torbjørn Birch Moltu
+ *
+ * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+ * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+ * http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
+ */
+
 package no.torbmol.util.collections;
 import java.io.Serializable;
 import java.lang.reflect.Array;
